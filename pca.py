8605baf7b18ef6c3e78d945ffe5aacb20aa3bba4
Raja Gangopadhya <rgangopadhya@ucla.edu>
2013-07-09 08:57:55 -0700

    porting displayData

--- pca.py
+++ pca.py
@@ -26,3 +26,35 @@ def recoverData(Z, U, K):
 	Applies the projection coefficients from Z to U to recover an approximation of X
 	"""	 
 	return np.dot(Z, np.transpose(U[:, :K]))
+
+def displayData(X):
+	"""
+	Display 2D data stored in X in a nice grid
+	"""
+	import math
+	example_width = math.round(math.sqrt(X.size[1]))
+
+	#find number of rows, columns
+	m, n = X.size
+	example_height = (n / example_width)
+
+	#find number of items to display
+	display_rows = math.floor(math.sqrt(m))
+	display_cols = math.ceil(m / display_rows)
+
+	#between image padding
+	pad = 1
+
+	#blank display
+	display_array = np.ones(pad + display_rows * (example_height + pad),
+						pad + display_cols * (example_width + pad))
+
+	curr_ex = 1
+	for j in xrange(0, display_rows):
+		for i in xrange(0, display_cols):
+			if curr_ex > m:
+				break
+
+			max_val = np.max(math.abs(X[curr_ex, :]))
+			display_array[(pad + j * example_height):(pad + )
+			, pad + i * example_width]	
