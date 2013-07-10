d77d80cef9dee111937ee014ea5bd9fb05c98817
Raja Gangopadhya <rgangopadhya@ucla.edu>
2013-07-10 00:34:57 -0700

    still working on display, not quite showing the faces correctly

--- pca.py
+++ pca.py
@@ -33,34 +33,34 @@ def displayData(X):
 	Display 2D data stored in X in a nice grid
 	"""
 	import math
-	example_width = math.round(math.sqrt(X.size[1]))
+	example_width = int(round(math.sqrt(X.shape[1])))
 
 	#find number of rows, columns
-	m, n = X.size
+	m, n = X.shape
 	example_height = (n / example_width)
 
 	#find number of items to display
-	display_rows = math.floor(math.sqrt(m))
-	display_cols = math.ceil(m / display_rows)
+	display_rows = int(math.floor(math.sqrt(m)))
+	display_cols = int(math.ceil(m / display_rows))
 
 	#between image padding
 	pad = 1
 
 	#blank display
-	display_array = np.ones(pad + display_rows * (example_height + pad),
-					pad + display_cols * (example_width + pad))
-
+	display_array = np.ones((pad + display_rows * (example_height + pad),\
+					pad + display_cols * (example_width + pad)))
 	curr_ex = 0
-	for j in xrange(0, display_rows):
-		for i in xrange(0, display_cols):
+	for j in xrange(display_rows):
+		for i in xrange(display_cols):
 			if curr_ex >= m:
 				break
-
-			max_val = np.max(math.abs(X[curr_ex, :]))
-			display_array[(pad + j * (example_height + pad)) + np.arange(example_height)
-			, pad + i * (example_width + pad) + np.arange(example_width)] \
-			= np.reshape(X[curr_ex, :], (example_height, example_width))
+			xr = pad + j * (example_height + pad) + np.arange(example_height)
+			yr = pad + i * (example_width + pad) + np.arange(example_width)		
+		
+			max_val = math.fabs(np.max(X[curr_ex, :]))
+			display_array[xr, :][:, yr]\
+			= np.reshape(X[curr_ex, :], (example_height, example_width))/max_val
 
 	pyplot.imshow(display_array)
 	pyplot.draw()	
-	pyplot.show(block=False)		
+	pyplot.show()		
